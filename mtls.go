30516847d6b8e007852159903e2726ac21af02b7
Stephen Fox <stephen.j.fox.jr@gmail.com>
2019-04-28 17:33:07 -0400

    Initial work on basic builder.

--- mtls.go
+++ mtls.go
@@ -1,17 +1,82 @@
 package mtls
 
 import (
+	"crypto"
+	"crypto/ecdsa"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"errors"
+	"fmt"
+	"io"
 	"math/big"
 	"net"
 	"os"
 	"time"
 )
+
+// BasicBuilder is a very basic mutual TLS certificate builder.
+type BasicBuilder struct {
+	TemplateCert   *x509.Certificate
+	ParentIfNotCa  *x509.Certificate
+	PrivateKey     crypto.Signer
+	Entropy        io.Reader
+	GenerateSerial bool
+}
+
+// BuildPemBlocks will build the certificate in the form of PEM blocks.
+func (o *BasicBuilder) BuildPemBlocks() (*PemBlocks, error) {
+	if o.TemplateCert == nil {
+		return nil, fmt.Errorf("template certificate cannot be nil")
+	}
+
+	if o.Entropy == nil {
+		return nil, fmt.Errorf("entropy cannot be nil")
+	}
+
+	if o.GenerateSerial {
+		serial, err := serialNumber(o.Entropy)
+		if err != nil {
+			return nil, err
+		}
+
+		o.TemplateCert.SerialNumber = serial
+	}
+
+	var parent *x509.Certificate
+	if o.TemplateCert.IsCA {
+		parent = o.TemplateCert
+	} else if o.ParentIfNotCa != nil {
+		parent = o.ParentIfNotCa
+	} else {
+		return nil, fmt.Errorf("template certificate is not a CA and a parent certificate was not provided")
+	}
+
+	certRaw, err := x509.CreateCertificate(o.Entropy, o.TemplateCert, parent, o.PrivateKey.Public(), o.PrivateKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate certificate from template - %s", err.Error())
+	}
+
+	keyBlock, err := privateKeyPemBlock(o.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PemBlocks{
+		Certificate: &pem.Block{
+			Type:  "CERTIFICATE",
+			Bytes: certRaw,
+		},
+		PrivateKey: keyBlock,
+	}, nil
+}
+
+type PemBlocks struct {
+	Certificate *pem.Block
+	PrivateKey  *pem.Block
+}
 
 // CreateFiles creates a certificate and private key pair for TLS mutual
 // authentication at the specified locations. If the specified files already
@@ -107,4 +172,37 @@ func CreateBlocks(organizationNames []string, optionalIps []net.IP, optionalDoma
 	}
 
 	return certificate, privateKey, nil
+}
+
+func serialNumber(entropy io.Reader) (*big.Int, error) {
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(entropy, serialNumberLimit)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate serial number - %s", err.Error())
+	}
+
+	return serialNumber, nil
+}
+
+func privateKeyPemBlock(privateKey crypto.Signer) (*pem.Block, error) {
+	switch typedKey := privateKey.(type) {
+	case *rsa.PrivateKey:
+		return &pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(typedKey),
+		}, nil
+	case *ecdsa.PrivateKey:
+		ecdsaRaw, err := x509.MarshalECPrivateKey(typedKey)
+		if err != nil {
+			return nil, err
+		}
+		return &pem.Block{
+			Type:  "EC PRIVATE KEY",
+			Bytes: ecdsaRaw,
+		}, nil
+	case nil:
+		return nil, fmt.Errorf("private key cannot be nil")
+	}
+
+	return nil, fmt.Errorf("unsupported private key type")
 }

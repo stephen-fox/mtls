c074b6692fe4f9c6f974722ad9c74c9e4b64bd6d
Stephen Fox <stephen.j.fox.jr@gmail.com>
2019-04-28 23:22:03 -0400

    Fixed signing a cert. w/ CA using builder.
    Added comments for the builder's fields for clarity.

--- mtls.go
+++ mtls.go
@@ -17,19 +17,57 @@ import (
 	"time"
 )
 
-// BasicBuilder is a very basic mutual TLS certificate builder.
+// BasicBuilder is a very basic TLS certificate builder. This is used to
+// generate certificates for mutual authentication.
 type BasicBuilder struct {
-	TemplateCert   *x509.Certificate
-	ParentIfNotCa  *x509.Certificate
-	PrivateKey     crypto.Signer
-	Entropy        io.Reader
+	// Template represents the certificate before it is built.
+	Template *x509.Certificate
+
+	// Parent is the parent (e.g., Certificate Authority) that will
+	// issue the certificate. This can be nil if the Template is
+	// a CA.
+	Parent *x509.Certificate
+
+	// KeyPair is the public-private key pair uniquely associated
+	// with the Template.
+	//
+	// This can be generated using the "GenerateKey" functions found
+	// in the 'ecdsa' and the 'rsa' packages. These functions will
+	// generate a private key. The crypto.PublicKey can be extracted
+	// from the private key structure.
+	KeyPair crypto.Signer
+
+	// TODO: Remove.
+	// PublicKey is the certificate's public key. This must be
+	// unique to this certificate.
+	//
+	// This can be generated using the "GenerateKey" functions found
+	// in the 'ecdsa' and the 'rsa' packages. These functions will
+	// generate a private key. The crypto.PublicKey can be extracted
+	// from the private key structure.
+	//PublicKey crypto.PublicKey
+
+	// Signer is the private key used to sign the Template. This is
+	// required when the Template is being issued by a CA.
+	Signer crypto.Signer
+
+	// Entropy is the source of entropy used when building
+	// the certificate.
+	Entropy io.Reader
+
+	// GenerateSerial, when true, will generate a new serial number
+	// for the certificate at build time.
 	GenerateSerial bool
 }
 
 // BuildPemBlocks will build the certificate in the form of PEM blocks.
 func (o *BasicBuilder) BuildPemBlocks() (*PemBlocks, error) {
-	if o.TemplateCert == nil {
+	if o.Template == nil {
 		return nil, fmt.Errorf("template certificate cannot be nil")
+	}
+
+	if o.KeyPair == nil {
+		return nil, fmt.Errorf("template key pair cannot be nil")
 	}
 
 	if o.Entropy == nil {
@@ -42,24 +80,28 @@ func (o *BasicBuilder) BuildPemBlocks() (*PemBlocks, error) {
 			return nil, err
 		}
 
-		o.TemplateCert.SerialNumber = serial
+		o.Template.SerialNumber = serial
 	}
 
 	var parent *x509.Certificate
-	if o.TemplateCert.IsCA {
-		parent = o.TemplateCert
-	} else if o.ParentIfNotCa != nil {
-		parent = o.ParentIfNotCa
+
+	if o.Template.IsCA {
+		parent = o.Template
+	} else if o.Parent != nil {
+		parent = o.Parent
+		if o.Signer == nil {
+			return nil, fmt.Errorf("signer cannot be nil when the template is being signed by a parent")
+		}
 	} else {
 		return nil, fmt.Errorf("template certificate is not a CA and a parent certificate was not provided")
 	}
 
-	certRaw, err := x509.CreateCertificate(o.Entropy, o.TemplateCert, parent, o.PrivateKey.Public(), o.PrivateKey)
+	certRaw, err := x509.CreateCertificate(o.Entropy, o.Template, parent, o.KeyPair.Public(), o.Signer)
 	if err != nil {
 		return nil, fmt.Errorf("failed to generate certificate from template - %s", err.Error())
 	}
 
-	keyBlock, err := privateKeyPemBlock(o.PrivateKey)
+	keyBlock, err := privateKeyPemBlock(o.KeyPair)
 	if err != nil {
 		return nil, err
 	}
